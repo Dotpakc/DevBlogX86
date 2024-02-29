4f60373df262ddd24e98f9a30c4627214d79c3aa
DotPack <lifirenko123@gmail.com>
2024-02-29 20:46:56 +0200

    setup db

--- core/settings.py
+++ core/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "devblogx86",
+        "USER": "devblogx86_user",
+        "PASSWORD": "1234",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
