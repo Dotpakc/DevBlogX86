f8aebd2efa50b6ac820b2a49a99512fb1eff2108
DotPack <lifirenko123@gmail.com>
2024-03-07 21:14:22 +0200

    fix

--- core/settings.py
+++ core/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    'easy_thumbnails',
-    
+        
     'apps.main',
     'apps.blog',
 ]
