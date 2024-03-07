362efe9ee50c180027781c1df9e8167c29573a82
DotPack <lifirenko123@gmail.com>
2024-03-07 21:13:48 +0200

    add Create post

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Post
-
+from .forms import PostForm
 # Create your views here.
 def index(request):
     
     all_posts = Post.objects.all()
     
     context = {
-        'all_posts': all_posts
+        'all_posts': all_posts,
+        'created_form': PostForm()
     }
     
     return render(request, 'blog/index.html', context)
@@ -22,3 +23,11 @@ def detail(request, post_id):
     }
     
     return render(request, 'blog/detail.html', context)
+
+def create_view(request):
+    
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save()
+            return redirect('blog:detail' , post_id=post.id)
--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    'easy_thumbnails',
+    
     'apps.main',
     'apps.blog',
 ]
