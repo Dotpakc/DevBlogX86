708e37ca4733bcfec050551287fac66f705aae56
DotPack <lifirenko123@gmail.com>
2024-03-14 20:06:57 +0200

    add Pagination

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,31 +1,38 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+#pagination
+from django.core.paginator import Paginator
 
 from .models import Post
 from .forms import PostForm
+
 # Create your views here.
+
 def index(request):
-    
+    # print(request.GET.get('page', 1))   
     all_posts = Post.objects.all()
-    
+    paginator = Paginator(all_posts, 3)
+    page = request.GET.get('page')
+    all_posts_page = paginator.get_page(page)
+        
     context = {
-        'all_posts': all_posts,
+        'all_posts': all_posts_page,
         'created_form': PostForm()
     }
-    
     return render(request, 'blog/index.html', context)
 
+
+@login_required
 def detail(request, post_id):
-    
     post = get_object_or_404(Post, pk=post_id)
     
     context = {
         'post': post
     }
-    
     return render(request, 'blog/detail.html', context)
 
+@login_required
 def create_view(request):
-    
     if request.method == 'POST':
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
