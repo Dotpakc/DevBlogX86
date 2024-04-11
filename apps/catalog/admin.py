103fd6fb6d2eee927c62f3b10664433f68424cc0
DotPack <lifirenko123@gmail.com>
2024-04-11 20:33:34 +0300

    Add Product and Image models to admin.py, and update index.html and views.py

--- apps/catalog/admin.py
+++ apps/catalog/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, Product, Image, ProductCategory
 # Register your models here.
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
+    
+
+class ProductCategoryInline(admin.TabularInline):
+    model = ProductCategory
+    extra = 1
+    
+class ImageInline(admin.TabularInline):
+    model = Image
+    fields = ('product', 'image', 'is_main')
+    extra = 1
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'quantity', 'price', 'created_at', 'updated_at')
+    list_filter = ('created_at', 'updated_at', 'category', 'quantity')
+    search_fields = ('title', 'description')
+    list_editable = ('quantity', 'price')
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [ProductCategoryInline, ImageInline]
--- apps/catalog/views.py
+++ apps/catalog/views.py
@@ -3,7 +3,7 @@ from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 
-from .models import Category
+from .models import Category, Product
 # Create your views here.
 
 class CatalogListView(ListView):
@@ -22,12 +22,15 @@ class ProductByCategoryView(ListView):
     
     def get_queryset(self):
        self.category = Category.objects.get(slug=self.kwargs['slug'])
-       self.parents_categories = Category.objects.filter(parent=self.category)
+       self.parents_categories = Category.objects.filter(parent=self.category).select_related('parent')
+       self.all_categories = self.parents_categories.get_descendants(include_self=True).values_list('id', flat=True) # вибираємо всі категорії, які є нащадками поточної категорії
+       self.products = Product.objects.filter(category__in=self.all_categories)
        return self.parents_categories
    
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['category'] = self.category
+        context['products'] = self.products
         return context
     
         
