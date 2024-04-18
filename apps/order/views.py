4468b38763044a437a9f8b849367b99ff8af986b
DotPack <lifirenko123@gmail.com>
2024-04-18 21:09:06 +0300

    Add 'apps.order' to INSTALLED_APPS in settings.py

--- apps/order/views.py
+++ apps/order/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.views.generic import View
+
+from .forms import CartForm
+
+class CartHandler(View):
+    def post(self, request):
+        form = CartForm(request.POST)
+        if form.is_valid():
+            cart = form.save(commit=False)
+            cart.user = request.user
+            cart.save()
+            return redirect('main:index')
+        
