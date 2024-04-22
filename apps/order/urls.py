00258a0b9d2ae4be9ca8012f83bccf28c43ea629
DotPack <lifirenko123@gmail.com>
2024-04-22 21:15:10 +0300

    Add cart functionality to order app

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name = 'order'
 
 urlpatterns = [
-       path('cart_add/', views.CartHandler.as_view(), name='cart_add'),
+       path('cart/', views.CartHandler.as_view(), name='cart'),
+       path('cart/add', views.CartAddHandler.as_view(), name='cart_add'),
+       path('cart/remove/<int:pk>', views.CartDeleteHandler.as_view(), name='cart_remove'),
 ]           
--- apps/order/views.py
+++ apps/order/views.py
@@ -1,15 +1,50 @@
 from django.shortcuts import render, redirect
 
-from django.views.generic import View
+from django.views.generic import View, ListView
+from django.contrib import messages
 
+from .models import Cart
 from .forms import CartForm
 
-class CartHandler(View):
+class CartAddHandler(View):
     def post(self, request):
         form = CartForm(request.POST)
         if form.is_valid():
             cart = form.save(commit=False)
-            cart.user = request.user
-            cart.save()
-            return redirect('main:index')
+            cart_item = Cart.objects.filter(product=cart.product, user=request.user).first()
+            if cart_item:
+                cart_item.quantity = cart.quantity
+                cart_item.save()    
+                messages.success(request, 'Кількість товару змінено')
+            else:
+                cart.user = request.user
+                cart.save()
+                messages.success(request, 'Товар додано до кошика')
+        else:
+            messages.error(request, f'Помилка додавання товару до кошика {form.errors}')
+        return redirect('order:cart')
+
         
+
+class CartHandler(ListView):
+    model = Cart
+    template_name = 'order/cart.html'
+    context_object_name = 'cart'
+    
+    
+    def get_queryset(self):
+        return Cart.objects.filter(user=self.request.user)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['get_total_cost'] = sum([item.get_total() for item in context['cart']])
+        return context
+    
+    
+class CartDeleteHandler(View):
+    def get(self, request, pk):
+        cart = Cart.objects.get(pk=pk, user=request.user)
+        cart.delete()
+        messages.success(request, 'Товар видалено з кошика')
+        return redirect('order:cart')
+    
