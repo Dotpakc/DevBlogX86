77363d073395336ad8c1a88d12e79a171f6930e4
Lifirenko Yegor <lifirenko123@gmail.com>
2024-05-16 20:26:08 +0300

    Update models, views, and templates for the main app

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.urls import reverse
+
+from django_ckeditor_5.fields import CKEditor5Field
 
 # Create your models here.
+class Page(models.Model):
+    title = models.CharField(verbose_name="Заголовок", max_length=255)
+    slug = models.SlugField(verbose_name="URL", max_length=255, unique=True)
+    content = CKEditor5Field('Текст', config_name='extends')
+    
+    sort = models.IntegerField(verbose_name="Сортировка", default=0)
+    
+    
+    class Meta:
+        ordering = ['sort']
+        verbose_name = "Страница"
+        verbose_name_plural = "Страницы"
+        
+    def get_absolute_url(self):
+        return reverse('main:page', kwargs={'slug': self.slug})
